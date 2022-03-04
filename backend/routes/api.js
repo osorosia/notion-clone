164a3ddaf0aa9a0fde58c851e26ebbcfc58ba5c6
osorosia <64416398+osorosia@users.noreply.github.com>
2022-03-05 01:42:53 +0900

    wip

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/hello', function(req, res, next) {
+  var param = {'result': 'Hello'};
+  res.header('Content-Type', 'application/json; charset=utf-8');
+  res.send(param);
+});
+
 module.exports = router;
