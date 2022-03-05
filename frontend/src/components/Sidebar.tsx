f7bd2037dd1e54cbe7ce03aebe33c80eebc78d26
osorosia <64416398+osorosia@users.noreply.github.com>
2022-03-05 18:30:54 +0900

    wip

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React from 'react';
+import Drawer from '@material-ui/core/Drawer';
+
+const First = () => {
+  return (
+    <React.Fragment>
+      <p>hello</p>
+    </React.Fragment>
+  );
+}
+
+const Second = () => {
+  return (
+    <React.Fragment>
+      <p>hello</p>
+    </React.Fragment>
+  );
+}
+
+const Third = () => {
+  return (
+    <React.Fragment>
+      <p>hello</p>
+    </React.Fragment>
+  );
+}
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div>
+      <Drawer anchor='left' variant='permanent'>
+        <First />
+        <Second />
+        <Third />
+      </Drawer>
+    </div>
   );
 };
 
