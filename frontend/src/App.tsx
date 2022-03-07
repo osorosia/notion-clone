6eb20cf2d90dba6e7216feecc44c7989d0b26e95
osorosia <64416398+osorosia@users.noreply.github.com>
2022-03-07 16:27:46 +0900

    wip デザイン検討

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,48 @@
 import React from 'react';
 import './App.scss';
-import { Note } from './Note';
 
 function App() {
+  const flag = false;
   return (
     <div className="App">
       {/* Sidebar------------------------------------------ */}
-      <div className="side">
+      <div
+        className="sidebar"
+        style={{display: flag ? 'none' : ''}}
+      >
+        {/* ヘッダー */}
+        <p>○○さんのNotion</p>
+
+        {/* メニュー上部 */}
         <ul>
-          <li>aaa</li>
-          <li>bbbb</li>
-          <li>ccccc</li>
+          <li>検索</li>
+          <li>更新一覧</li>
+          <li>設定</li>
         </ul>
+
+        {/* ノート一覧 */}
         <ul>
-          <li>aaa</li>
-          <li>bbbb</li>
-          <li>ccccc</li>
+          <li>使ってみる</li>
+          <li>クイックメモ</li>
+          <li>タスク管理</li>
         </ul>
+
+        {/* メニュー下部 */}
+        <ul>
+          <li>テンプレート</li>
+          <li>インポート</li>
+          <li>ゴミ箱</li>
+        </ul>
+
       </div>
 
       
-      {/* Header------------------------------------------- */}
-
-      
-      {/* Main--------------------------------------------- */}
+      {/* Content------------------------------------------- */}
       <div className="content">
+        {/* ヘッダー */}
+        <p>title</p>
+        {/* 本文 */}
+        <p>title</p>
         <ul>
           <li>aaa</li>
           <li>bbbb</li>
