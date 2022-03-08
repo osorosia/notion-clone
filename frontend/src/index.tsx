06ebfcf277610a9b12f06af2a25bb8f174d0afc9
osorosia <64416398+osorosia@users.noreply.github.com>
2022-03-08 17:13:18 +0900

    Router導入

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -11,7 +11,9 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path='/' element={<App />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
@@ -20,4 +22,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
