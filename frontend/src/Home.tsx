443c57842cf75bc8e8b3ad66ec169726d1dd0522
osorosia <64416398+osorosia@users.noreply.github.com>
2022-03-12 21:49:20 +0900

    ノート一覧を取得(logのみ)

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './Home.scss';
 
 type Line = {
   text: string;
@@ -26,8 +28,21 @@ const Home = () => {
   // ノート一覧
   const [notes, setNotes] = useState<Note[]>();
 
+  // ノート一覧を取得
+  useEffect(() => {
+    const fetchGet = async () => {
+      const url = "http://localhost:8080/api/note";
+      const res = await axios.get(url);
+      const json = res.data;
+
+      console.log('setNotes()', json);
+      setNotes(json);
+    }
+    fetchGet();
+  }, []);
+
   return (
-    <div className='Tmp'>
+    <div className='Home'>
       {/* サイドバー */}
       <div className='sidebar'>
         {/* ヘッダー */}
