78f8468d5991684dcf907791a32c6dc9b83c4704
osorosia <64416398+osorosia@users.noreply.github.com>
2022-03-16 15:23:46 +0900

    modelを変更

--- backend/models/note.js
+++ backend/models/note.js
@@ -1,12 +1,23 @@
 const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const NoteSchema = mongoose.Schema({
-    title: String,
+    title: {type: String, default: ''},
+    note_id: Number,
     body: [
         {
-            text: String,
+            text: {type: String, default: ''},
+            style: {type: String, default: ''},
+            font: {
+                bold: {type: Boolean, default: false},
+                underline: {type: Boolean, default: false},
+                border: {type: Boolean, default: false},
+                itaric: {type: Boolean, default: false},
+                strike: {type: Boolean, default: false},
+            },
         }
     ],
 });
 
+NoteSchema.plugin(AutoIncrement, { inc_field: 'note_id' });
 module.exports = mongoose.model('Note', NoteSchema);
